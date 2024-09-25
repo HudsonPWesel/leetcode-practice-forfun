f1e9824baed9b049e28aee06e2c60c398d7eef20
HudsonPWesel <cswyld7k@gmail.com>
2024-09-25 14:11:25 -0400

    Resolve groupAnagrams

--- Arrays/groupAnagrams.py
+++ Arrays/groupAnagrams.py
@@ -3,14 +3,15 @@ from typing import List
 # input: strs = ["act","pots","tops","cat","stop","hat"]Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
 
 
+# Reattmpt of groupAnagrams
 def groupAnagrams(strs: List[str]) -> List[List[str]]:
     res = defaultdict(list)
     for s in strs:
         count = [0] * 26
         for c in s:
-            count[ord(c) - ord("a")] += 1
+            count[ord(c) - ord('a')] += 1
         res[tuple(count)].append(s)
-    return res.values()
+    return list(res.values())
 
 
 print(groupAnagrams(["act", "pots", "tops", "cat", "stop", "hat"]))
