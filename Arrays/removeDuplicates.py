4f5ab73980064946d4c10692432232934852df3e
HudsonPWesel <cswyld7k@gmail.com>
2024-09-30 12:17:29 -0400

    trying-again-conatinsDuplicates

--- Arrays/removeDuplicates.py
+++ Arrays/removeDuplicates.py
@@ -1,16 +1,14 @@
 def removeDuplicates(nums):
-    res = 1
     for i in range(len(nums)):
-        if nums[i] == i + 1:
-            res += 1
+        if nums[i] == i + 1 or (nums[i] == 0 and i == 0):
             continue
         else:
-            if not (nums[-1] - nums[i]):
-                return res
+            if not (nums[-1] - i):
+                nums[i] = nums[-1]
+                print(nums)
+                return i + 1
             else:
-                nums[i] = nums[i] + 1
-    print(nums)
-    return res
+                nums[i] = i
 
 
-print(removeDuplicates([1, 1, 2]))
+print(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
