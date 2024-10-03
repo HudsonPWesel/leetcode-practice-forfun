8403c7ea21be9c2493c914bc575e1d055560a9bc
HudsonPWesel <cswyld7k@gmail.com>
2024-10-03 09:10:40 -0400

    Not working sol

---
+++ plusOne.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+def plusOne(digits: List[int]) -> List[int]:
+    carry, i = 1, len(digits) - 1
+    while carry:
+        if i < len(digits):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                carry = 0
+        else:
+            digits.insert(0, 1)
+            carry = 0
+        i -= 1
+    return digits[::-1]
+
+
+print(plusOne([9, 9, 9]))
